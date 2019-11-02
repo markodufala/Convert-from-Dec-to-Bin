77568d94f8f765818b1b737b8ad004d831a6e62d
Marko Dufala <50919055+nameMark@users.noreply.github.com>
2019-11-02 20:35:02 +0100

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -1,15 +1,15 @@
 let veryFirstDecimalNumber = 130  // Your number in Decimal
 var decimalNumber = veryFirstDecimalNumber
 var binaryNumberString = ""
-var steps = 0
+var digits = 0
 
 for _ in stride(from: 0, to: 1000, by: 1){  //for loop for 1000 Binar digits
 
     if decimalNumber != 0 {
     binaryNumberString = "\(decimalNumber % 2)" + binaryNumberString  // division with remnant or not
-    steps += 1                                                        //digit(s) counting
+    digits += 1                                                        //digit(s) counting
     }
     decimalNumber = decimalNumber / 2                                 // dividing nummber by 2
 }
 
-print("Dec(\(veryFirstDecimalNumber)) == Bin(\(binaryNumberString)), after \(steps) steps.") // printing a message 
+print("Dec(\(veryFirstDecimalNumber)) == Bin(\(binaryNumberString)), with \(digits) digit's.") // printing a message
