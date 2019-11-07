fef491f7b3c8800ab7f6f49b19a30b83c5a9ed27
Marko Dufala <50919055+nameMark@users.noreply.github.com>
2019-11-07 21:06:12 +0100

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -7,7 +7,7 @@ for _ in stride(from: 0, to: 1000, by: 1){  //for loop for 1000 Binar digits
 
     if decimalNumber != 0 {
     binaryNumberString = "\(decimalNumber % 2)" + binaryNumberString  // division with remnant or not
-    digits += 1                                                        //digit(s) counting
+    digits += 1                                                       //digit(s) counting
     }
     decimalNumber = decimalNumber / 2                                 // dividing nummber by 2
 }
